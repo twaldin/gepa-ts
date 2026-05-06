cf541d4f5575065dfb6d023df050d5a5704b9642
twaldin <timothy@waldin.net>
2026-05-05 23:11:27 -0700

    node: gepa-ts-v1-port-execute-types-and-utils-coder final state

--- src/stoppers.ts
+++ src/stoppers.ts
@@ -1,24 +1,38 @@
 import type { GEPAStateLike, Stopper } from "./types";
 
-type CallableMaxMetricCallsStopper = MaxMetricCallsStopper & Stopper;
-type CallableCompositeStopper = CompositeStopper & Stopper;
+export interface MaxMetricCallsStopper extends Stopper {
+  readonly max_metric_calls: number;
+}
 
-export class MaxMetricCallsStopper {
+class MaxMetricCallsStopperImpl {
   readonly max_metric_calls: number;
 
   constructor(max_metric_calls: number) {
     this.max_metric_calls = max_metric_calls;
 
-    const callable: CallableMaxMetricCallsStopper = Object.assign(
+    const callable: MaxMetricCallsStopper = Object.assign(
       (state: GEPAStateLike) => state.total_num_evals >= max_metric_calls,
       { max_metric_calls },
     );
-    Object.setPrototypeOf(callable, MaxMetricCallsStopper.prototype);
+    Object.setPrototypeOf(callable, MaxMetricCallsStopperImpl.prototype);
     return callable;
   }
 }
 
-export class CompositeStopper {
+export const MaxMetricCallsStopper: {
+  new (max_metric_calls: number): MaxMetricCallsStopper;
+  prototype: MaxMetricCallsStopper;
+} = MaxMetricCallsStopperImpl as {
+  new (max_metric_calls: number): MaxMetricCallsStopper;
+  prototype: MaxMetricCallsStopper;
+};
+
+export interface CompositeStopper extends Stopper {
+  readonly stoppers: Stopper[];
+  readonly mode: "any" | "all";
+}
+
+class CompositeStopperImpl {
   readonly stoppers: Stopper[];
   readonly mode: "any" | "all";
 
@@ -31,7 +45,7 @@ export class CompositeStopper {
     this.mode = mode;
     this.stoppers = stoppers;
 
-    const callable: CallableCompositeStopper = Object.assign(
+    const callable: CompositeStopper = Object.assign(
       (state: GEPAStateLike) => {
         if (mode === "any") {
           return stoppers.some((stopper) => stopper(state));
@@ -43,7 +57,15 @@ export class CompositeStopper {
         stoppers,
       },
     );
-    Object.setPrototypeOf(callable, CompositeStopper.prototype);
+    Object.setPrototypeOf(callable, CompositeStopperImpl.prototype);
     return callable;
   }
 }
+
+export const CompositeStopper: {
+  new (...args: Array<Stopper | "any" | "all">): CompositeStopper;
+  prototype: CompositeStopper;
+} = CompositeStopperImpl as {
+  new (...args: Array<Stopper | "any" | "all">): CompositeStopper;
+  prototype: CompositeStopper;
+};
